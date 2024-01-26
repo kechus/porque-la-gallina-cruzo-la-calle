d1d6c7fe8d8ccef26bfb08759ef4093217641346
Samuelastro255 <samuel.padilla45@gmail.com>
2024-01-26 12:57:42 -0600

    Gallina animacion

--- Assets/Scripts/Player/Gallina.cs
+++ Assets/Scripts/Player/Gallina.cs
@@ -6,12 +6,15 @@ public class Gallina : MonoBehaviour
 {
     Vector2 movement;
     Rigidbody2D RB;
-    private const float MOVE_VELOCITY = 15f;
+    Animator animator;
+    private const float MOVE_VELOCITY = 5f;
+    bool die = false;
 
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,7 +25,32 @@ public class Gallina : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         //Movimiento basico
-        movement.Normalize();
-        RB.MovePosition(RB.position + (Time.fixedDeltaTime * MOVE_VELOCITY * movement));
+        if(!die) {
+            movement.Normalize();
+            RB.MovePosition(RB.position + (Time.fixedDeltaTime * MOVE_VELOCITY * movement));
+
+            if (movement.x != 0 || movement.y != 0)
+            {
+                animator.SetBool("Move", true);
+            }
+            else
+            {
+                animator.SetBool("Move", false);
+            }
+
+            if (movement.x != 0)
+            {
+                this.gameObject.transform.rotation = Quaternion.Euler(0f, movement.x < 0 ? 180f : 0f, 0f);
+            }
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Carro")
+        {
+            animator.SetTrigger("Die");
+            die = true;
+        }
     }
 }
