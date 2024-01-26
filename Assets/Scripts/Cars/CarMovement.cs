584f8822c65fbe61d0e4a2cb58447ecc5449583e
Jesus Antonio <kechus4556@gmail.com>
2024-01-26 11:39:37 -0600

    fix compilation error

--- Assets/Scripts/Cars/CarMovement.cs
+++ Assets/Scripts/Cars/CarMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CarMovement : MonoBehaviour
@@ -8,16 +6,10 @@ public class CarMovement : MonoBehaviour
 
     void Start()
     {
-        
     }
 
-    void fixedUpdate()
+    void FixedUpdate()
     {
-        transform.position += transform.forward * SPEED * Time.deltaTime;
-    }
-
-    void Update()
-    {
-        
+        transform.Translate(new Vector3(0,-1,0) * SPEED * Time.deltaTime);
     }
 }
